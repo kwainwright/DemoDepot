ce69d5ccc13e1f891f01b33b1875e046d19670a8
unknown <kwainwri@.njmrsnet-aegis.com>
2016-06-28 17:20:55 -0400

    Finish up login/logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
 class SessionsController < ApplicationController
+  skip_before_action :authorize
+  
   def new
   end
 
   def create
     user = User.find_by(name: params[:name])
-    if user and user.authenticate(params[:name])
+    if user and user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to admin_url
     else
